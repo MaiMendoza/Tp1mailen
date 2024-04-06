454e96155e9a0c4ec2e80953bc585167bd0335b8
LENOVO <LENOVO@192.168.0.7>
2024-04-05 22:48:15 -0300

    Asistencia Mailen Emilce Mendoza

---
+++ Proyecto1/src/ar/edu/unju/pv/ejemplo/Prueba.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.pv.ejemplo;
+
+
+//Ejercicio 1//
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1= 8, num2= 5, num3= 1,num4= 20, num5= 9;
+		double sum= num1+num2+num3+num4+num5;
+		 double promedio= sum/5;
+        System.out.print("El promedio es: "+ promedio);
+		
+	}
+
+}
